1c17b1fa150257121c5403cfb70c0d6c5e47d74d
Emilio Gustavo Miranda Barbosa <emilio.barbosa@olsen.odo.br>
2022-09-22 18:48:55 -0300

    refazendo tela de cadastro de animais

--- src/pages/AnimalSignUp.vue
+++ src/pages/AnimalSignUp.vue
@@ -1,18 +1,5 @@
 <template>
   <div class="container">
-    <div class="titulo">
-      <h4 class="text-bold">Dados do Animal</h4>
-    </div>
-    <div class="input1">
-      <q-input outlined label="Usuário" class="q-mb-md" bg-color="white"/>
-    </div>
-    <div class="input2">
-      <q-input outlined label="Usuário" class="q-mb-md" bg-color="white"/>
-    </div>
-    <div class="input3">
-      <q-input outlined label="Usuário" class="q-mb-md" bg-color="white"/>
-    </div>
-
 
   </div>
 </template>
@@ -24,21 +11,5 @@ export default {
 </script>
 
 <style scoped>
-.container{
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  margin: 50px;
-}
-.input1{
-  width: 80%;
-  grid-column: 1;
-}
-.input2{
-  width: 80%;
-  grid-column: 2;
-}
-.input3{
-  width: 80%;
-  grid-column: 3;
-}
+
 </style>
