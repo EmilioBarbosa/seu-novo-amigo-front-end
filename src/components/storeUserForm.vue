c284ea5263567e497382aa939dc8c15e52aa9c6b
Emilio Barbosa <emiliobarbos4@gmail.com>
2022-11-03 22:01:39 -0300

    fazendo req para armazenar user

--- src/components/storeUserForm.vue
+++ src/components/storeUserForm.vue
@@ -1,28 +1,28 @@
 <template>
-  <q-form>
-    <div class="form">
+  <q-form @submit.prevent="submit">
+    <div class="form" >
       <div class="name">
-        <q-input outlined bg-color="white" dense label="Nome"/>
+        <q-input outlined bg-color="white" v-model="name" dense label="Nome"/>
       </div>
       <div class="phone flex flex-center">
         <div class="input-phone">
-          <q-input outlined bg-color="white" dense label="Telefone"/>
+          <q-input outlined bg-color="white" v-model="phone" dense label="Telefone"/>
         </div>
         <div class="phone-wpp text-center">
-          <q-checkbox  left-label label="Telefone com Whatsapp ?" />
+          <q-checkbox v-model="phoneWpp"  left-label label="Telefone com Whatsapp ?" />
         </div>
       </div>
       <div class="email">
-        <q-input outlined bg-color="white" dense label="E-mail"/>
+        <q-input v-model="email" outlined bg-color="white" dense label="E-mail"/>
       </div>
       <div class="email-confirm">
-        <q-input outlined bg-color="white" dense label="Confirme seu e-mail"/>
+        <q-input v-model="emailConfirm" outlined bg-color="white" dense label="Confirme seu e-mail"/>
       </div>
       <div class="password">
-        <q-input outlined type="password" bg-color="white" dense label="Senha"/>
+        <q-input v-model="password" outlined type="password" bg-color="white" dense label="Senha"/>
       </div>
       <div class="password-confirm">
-        <q-input outlined type="password" bg-color="white" dense label="Confirme sua senha"/>
+        <q-input v-model="passwordConfirm" outlined type="password" bg-color="white" dense label="Confirme sua senha"/>
       </div>
       <div class="state">
         <q-select
@@ -34,7 +34,6 @@
           :options="states"
           option-label="name"
           option-value="id"
-          @change="clearCitySelected"
         >
           <template v-slot:option="scope">
             <q-item v-bind="scope.itemProps">
@@ -57,13 +56,13 @@
         </q-select>
       </div>
       <div class="neighborhood">
-        <q-input outlined bg-color="white" dense label="Bairro"/>
+        <q-input v-model="neighborhood" outlined bg-color="white" dense label="Bairro"/>
       </div>
       <div class="street">
-        <q-input outlined bg-color="white" dense label="Logradouro"/>
+        <q-input v-model="street" outlined bg-color="white" dense label="Logradouro"/>
       </div>
       <div class="about">
-        <q-input type="textarea" outlined bg-color="white" dense label="Sobre você"/>
+        <q-input v-model="about" type="textarea" outlined bg-color="white" dense label="Sobre você"/>
       </div>
     </div>
     <div class="text-center">
@@ -89,6 +88,17 @@ import {computed, onMounted, ref} from "vue";
 
 const stateSelected = ref(null);
 const citySelected = ref(null);
+const name = ref(null);
+const phone = ref(null);
+const phoneWpp = ref(null);
+const email = ref(null);
+const emailConfirm = ref(null);
+const password = ref(null);
+const passwordConfirm = ref(null);
+const neighborhood = ref(null);
+const street = ref(null);
+const about = ref(null);
+
 const states = ref(null);
 const cities = ref(null);
 
@@ -100,8 +110,26 @@ const citiesOfSelectedState = computed(() => {
    }
 })
 
-function clearCitySelected() {
-  alert('mudou')
+function submit() {
+  api.post('/users', {
+    'name': name.value,
+    'email': email.value,
+    'email_confirmation': emailConfirm.value,
+    'description': about.value,
+    'password': password.value,
+    'password_confirmation': passwordConfirm.value,
+    'phone_number': phone.value,
+    'phone_number_whatsapp': phoneWpp.value,
+    'street': street.value,
+    'neighborhood': neighborhood.value,
+    'city_id': citySelected.value.id
+  })
+  .then(response =>{
+    console.log(response)
+  }).catch(response => {
+    console.log(response)
+  })
+
 }
 function getStates() {
   api.get('/states').then(response => {
@@ -133,7 +161,6 @@ onMounted(() => {
   'about about';
   gap: 5px;
   padding: 20px;
-  color: black;
 }
 .name{
   grid-area: name;
